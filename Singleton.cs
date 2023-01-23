fa6025f200fd67207a34c9dd24194661e91c70f4
Victor Gheno <victorgheno@hotmail.com>
2023-01-22 22:32:17 -0300

    Add files via upload

---
+++ Singleton.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour
+    where T : Singleton<T>
+{
+    public static T Instance { get; private set; }
+
+    public virtual void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = (T)this;
+            DontDestroyOnLoad(this);
+        }
+    }
+}
